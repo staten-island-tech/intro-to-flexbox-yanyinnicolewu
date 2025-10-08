c03a4533e9d73190a262a87587a842743c3c43f6
yanyinnicolewu <nicolewu288@gmail.com>
2025-10-08 09:33:02 -0700

    xdgd

---
+++ app.js
@@ -0,0 +1,51 @@
+const dogs = [
+  {
+    name: "Golden Retriever",
+    price: 2250,
+    inStock: true,
+    img: "https://images.photowall.com/products/57215/golden-retriever-puppy.jpg?h=699&q=85",
+    alt: "The Golden Retriever is a Scottish breed of retriever dog of medium size. It is characterised by a gentle and affectionate nature and a striking golden coat. It is a working dog, and registration is subject to successful completion of a working trial.",
+  }
+
+  {
+    name: "Border Collie",
+    price: 2150,
+    inStock: true,
+    img: "https://pethelpful.com/.image/w_3840,q_auto:good,c_fill,ar_4:3/MTk2NTc4NDk3MDY0MzQ3NDU0/choosing-a-border-collie-pros-and-cons.png",
+    alt: "The Border Collie is a British breed of herding dog of the collie type of medium size. It originates in the region of the Anglo-Scottish border, and descends from the traditional sheepdogs once found all over the British Isles. It is kept mostly as a working sheep-herding dog or as a companion animal",
+  }
+
+  {
+    name: "Corgi",
+    price: 1800,
+    inStock: true,
+    img: "https://cdn.britannica.com/80/232780-050-404D6708/Pembroke-welsh-corgi-dog.jpg",
+    alt: "The Pembroke Welsh Corgi is a cattle herding dog breed that originated in Pembrokeshire, Wales. The name Corgi is of Welsh origin, and is a compound of the words cor and ci. It is one of two breeds known as a Welsh Corgi, the other being the Cardigan Welsh Corgi",
+  }
+
+  {
+    name: "",
+    price: ,
+    inStock: ,
+    img: "",
+    alt: "",
+  }
+
+  {
+    name: "",
+    price: ,
+    inStock: ,
+    img: "",
+    alt: "",
+  }
+
+  {
+    name: "",
+    price: ,
+    inStock: ,
+    img: "",
+    alt: "",
+  }
+  
+
+];
