563d338ca8074d7ec43dbce3e643d57565ff7886
yanyinnicolewu <nicolewu288@gmail.com>
2025-10-14 12:32:57 -0400

    nb

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dogs = [
     name: "Golden Retriever",
     price: 2250,
     inStock: true,
-    img: "https://images.photowall.com/products/57215/golden-retriever-puppy.jpg?h=699&q=85",
+    img: "Golden Retriever.png",
     alt: "The Golden Retriever is a Scottish breed of retriever dog of medium size. It is characterised by a gentle and affectionate nature and a striking golden coat. It is a working dog, and registration is subject to successful completion of a working trial.",
   },
 
@@ -11,7 +11,7 @@ const dogs = [
     name: "Border Collie",
     price: 2150,
     inStock: true,
-    img: "https://pethelpful.com/.image/w_3840,q_auto:good,c_fill,ar_4:3/MTk2NTc4NDk3MDY0MzQ3NDU0/choosing-a-border-collie-pros-and-cons.png",
+    img: "Border Collie.png",
     alt: "The Border Collie is a British breed of herding dog of the collie type of medium size. It originates in the region of the Anglo-Scottish border, and descends from the traditional sheepdogs once found all over the British Isles. It is kept mostly as a working sheep-herding dog or as a companion animal.",
   },
 
@@ -19,7 +19,7 @@ const dogs = [
     name: "Corgi",
     price: 1800,
     inStock: true,
-    img: "https://cdn.britannica.com/80/232780-050-404D6708/Pembroke-welsh-corgi-dog.jpg",
+    img: "Corgi.png",
     alt: "The Pembroke Welsh Corgi is a cattle herding dog breed that originated in Pembrokeshire, Wales. The name Corgi is of Welsh origin, and is a compound of the words cor and ci. It is one of two breeds known as a Welsh Corgi, the other being the Cardigan Welsh Corgi.",
   },
 
@@ -27,7 +27,7 @@ const dogs = [
     name: "Husky",
     price: 1190,
     inStock: true,
-    img: "https://cdn.britannica.com/84/232784-050-1769B477/Siberian-Husky-dog.jpg",
+    img: "Husky.png",
     alt: "Husky is a general term for a type of dog used in the polar regions, primarily and specifically for work as sled dogs. It refers to a traditional northern type, notable for its cold-weather tolerance and overall hardiness.",
   },
 
@@ -35,7 +35,7 @@ const dogs = [
     name: "Akita",
     price: 2500,
     inStock: true,
-    img: "https://st.depositphotos.com/2012859/2355/i/450 depositphotos_23554637-stock-photo-akta-inu-dog.jpg",
+    img: "Akita.png",
     alt: "The Akita is a Japanese dog breed of large size. Originating from the mountains of northern Japan, the Akita has a short double coat similar to that of many other northern spitz breeds. ",
   },
 
@@ -43,7 +43,7 @@ const dogs = [
     name: "Chow Chow",
     price: 2600,
     inStock: true,
-    img: "https://www.borrowmydoggy.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F4ij0poqn%2Fproduction%2Fa526190ae7cb98cccd7dd3a1a4caf127702a6cdf-800x600.jpg&w=1080&q=80",
+    img: "Chow Chow.png",
     alt: "The Chow Chow is a spitz-type of dog breed originally from Northern China. The Chow Chow is a sturdily built dog, square in profile, with a broad skull and small, triangular, erect ears with rounded tips. The breed is known for a very dense double coat that is either smooth or rough.",
   },
 
@@ -51,7 +51,7 @@ const dogs = [
     name: "Doberman",
     price: 2000,
     inStock: true,
-    img: "https://images.ctfassets.net/nx3pzsky0bc9/5JNIP0KkbMkF8sjyeVFcvt/d2db4e388b752e69935a6001b0259efa/Doberman_feature_image.png?w=804",
+    img: "Doberman.png",
     alt: "The Dobermann is a German breed of medium-large working dog of pinscher type. It was originally bred in Thuringia in about 1890 by Louis Dobermann, a tax collector. It has a long muzzle and ideally an even and graceful gait.",
   },
 
@@ -59,7 +59,7 @@ const dogs = [
     name: "Labrador",
     price: 1650,
     inStock: true,
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Black_labrador_puppy_%282754841728%29.jpg/2560px-Black_labrador_puppy_%282754841728%29.jpg",
+    img: "Labrador.png",
     alt: "The Labrador Retriever, also known simply as the Labrador or Lab, is a British breed of retriever gun dog. It was developed in the United Kingdom from St. John's water dogs imported from the colony of Newfoundland, and was named after the Labrador region of that colony.",
   },
 
@@ -67,7 +67,7 @@ const dogs = [
     name: "Rottweilers",
     price: 2000,
     inStock: true,
-    img: "https://consumer-cms.petfinder.com/sites/default/files/images/content/Rottweiler%204.jpg",
+    img: "Rottweilers.png",
     alt: "The Rottweiler is a breed of domestic mastiff type dog, regarded as medium-to-large or large. The dogs were known in German as Rottweiler Metzgerhund, meaning Rottweil butchers' dogs, because their main use was to herd livestock and pull carts laden with butchered meat to market.",
   },
 
@@ -75,7 +75,7 @@ const dogs = [
     name: "German Shepherd",
     price: 3000,
     inStock: true,
-    img: "https://pet-health-content-media.chewy.com/wp-content/uploads/2024/09/11180014/202105German-Shepherd-1358309706-scaled-1.jpg",
+    img: "German Shepherd.png",
     alt: "The German Shepherd, also known in Britain as an Alsatian, is a German breed of working dog of medium to large size. The breed was developed by Max von Stephanitz using various traditional German herding dogs from 1899. It was originally bred as a herding dog, for herding sheep.",
   },
 
@@ -83,7 +83,7 @@ const dogs = [
     name: "Dachshund",
     price: 1250,
     inStock: true,
-    img: "https://www.rover.com/blog/wp-content/uploads/dachshund-breed-1024x576.jpg",
+    img: "Dachshund.png",
     alt: "The dachshund, also known as the wiener dog, or sausage dog, badger dog, doxen and doxie, is a short-legged, long-bodied, hound-type dog breed. The dog may be smooth-haired, wire-haired, or long-haired, with varied coloration.",
   },
 
@@ -91,7 +91,7 @@ const dogs = [
     name: "Dalmatian",
     price: 2000,
     inStock: true,
-    img: "https://media.istockphoto.com/id/500851439/photo/dalmatian-dog-isolated-on-white.jpg?s=612x612&w=0&k=20&c=bXAdIw3thvMADSpEbQFOGz4_ftZRBSPUDcuz-zuIve4=",
+    img: "Dalmatian.png",
     alt: "The Dalmatian is a breed of dog with a white coat marked with dark-coloured spots. Originally bred as a hunting dog, it was also used as a carriage dog, notably by firefighters in the days of horse-drawn firehouse carriages. The origins of this breed can be traced back to Croatia and its historical region of Dalmatia.",
   },
 
@@ -99,7 +99,7 @@ const dogs = [
     name: "Bulldog",
     price: 2750,
     inStock: true,
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Bulldog_inglese.jpg/1200px-Bulldog_inglese.jpg",
+    img: "Bulldog.png",
     alt: "The Bulldog is a British breed of dog of mastiff type. It may also be known as the English Bulldog or British Bulldog. It is a stocky, muscular dog of medium size, with a large head, thick folds of skin around the face and shoulders and a relatively flat face with a protruding lower jaw.",
   },
 
@@ -107,7 +107,7 @@ const dogs = [
     name: "Chihuahua",
     price: 1750,
     inStock: true,
-    img: "https://cdn.britannica.com/44/233244-050-A65D4571/Chihuahua-dog.jpg",
+    img: "Chihuahua.png",
     alt: "The Chihuahua is a Mexican breed of toy dog. It is named for the Mexican state of Chihuahua and is one of the smallest dog breeds in the world. It is usually kept as a companion animal.",
   },
 
@@ -115,7 +115,7 @@ const dogs = [
     name: "Great Dane",
     price: 1500,
     inStock: true,
-    img: "https://www.zooplus.ie/magazine/wp-content/uploads/2018/11/Great-Dane-768x512.webp",
+    img: "Great Dane.png",
     alt: "The Great Dane is a German breed of large mastiff-sighthound, which descends from hunting dogs of the Middle Ages used to hunt bears, wild boar, and deer. They were also used as guardian dogs of German nobility. It is one of the two largest dog breeds in the world, along with the Irish Wolfhound.",
   },
 
@@ -123,7 +123,7 @@ const dogs = [
     name: "Havanese",
     price: 3250,
     inStock: true,
-    img: "https://apupabove.com/cdn/shop/articles/Havanese.webp?v=1742407835&width=1600",
+    img: "Havanese.png",
     alt: "The Havanese, a bichon-type dog, is the national dog of Cuba, developed from the now extinct Blanquito de la Habana. The Blanquito descended from the also now-extinct Bichón Tenerife.",
   },
 
@@ -131,7 +131,7 @@ const dogs = [
     name: "Brittany",
     price: 2000,
     inStock: true,
-    img: "https://cdn-fastly.petguide.com/media/2022/02/28/8263516/brittany.jpg?size=720x845&nocrop=1",
+    img: "Brittany.png",
     alt: "The Brittany Spaniel or Breton Spaniel is a French breed of gun dog of spaniel type, bred primarily for bird hunting. It developed between the seventeenth and nineteenth centuries in Brittany in north-west France, and was officially recognized early in the twentieth.",
   },
 
@@ -139,7 +139,7 @@ const dogs = [
     name: "Poodle",
     price: 1900,
     inStock: true,
-    img: "https://www.dogster.com/wp-content/uploads/2023/08/red-toy-poodle-in-the-park_Mykhaylo_Kozelko_Shutterstock.jpg",
+    img: "Poodle.png",
     alt: "The Poodle, called the Pudel in German and the Caniche in French, is a breed of water dog. The breed is divided into four varieties based on size, the Standard Poodle, Medium Poodle, Miniature Poodle and Toy Poodle, although the Medium Poodle is not universally recognised.",
   },
 
@@ -147,7 +147,7 @@ const dogs = [
     name: "Boxer",
     price: 2150,
     inStock: true,
-    img: "https://www.thesprucepets.com/thmb/YwjpUBfdG8mkz2L64CX4-mA8cko=/1539x0/filters:no_upscale():strip_icc()/boxer-dog-breed-1117944-hero-dfe9f67a59ce4ab19ebd274c06b28ad1.jpg",
+    img: "Boxer.png",
     alt: "The Boxer is a medium to large, short-haired dog breed of mastiff-type, developed in Germany. The coat is smooth and tight-fitting; colors are fawn or brindled, with or without white markings.",
   },
 
@@ -155,15 +155,15 @@ const dogs = [
     name: "Pug",
     price: 1400,
     inStock: true,
-    img: "https://cdn.britannica.com/34/233234-050-1649BFA9/Pug-dog.jpg?w=400&h=300&c=crop",
+    img: "Pug.png",
     alt: "The Pug is a breed of dog with the physically distinctive features of a wrinkly, short-muzzled face, and curled tail.",
   },
 ];
 
 function inject(item) {
-  let name = "Roman";
-  const container = document.querySelector(".name");
-  container.insertAdjacentHTML("afterbegin");
+  let name = "Dog sale";
+  const container = document.querySelector(".container");
+  container.insertAdjacentHTML("afterbegin", `<h1>${name}</h1>`);
 }
 
 inject(product[0]);
